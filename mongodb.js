d75979cdab4154445bb9fcc4c6dc2e2ead0f3ffc
Daniel-Sogbey <mathematics06physics@gmail.com>
2020-12-22 22:50:56 -0800

    added methods

--- mongodb.js
+++ mongodb.js
@@ -17,69 +17,90 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    // db
-    //   .collection("users")
-    //   .findOne(
-    // { _id: new ObjectID("5fe235b00eec89315ca226ab") },
-    // (error, user) => {
-    //   if (error) {
-    // return console.log("Unable to fetch user");
-    //   }
-    //
-    //   if (user == null) {
-    // return console.log("user not found");
-    //   }
-    //
-    //   console.log(user);
-    // }
-    //   );
+    db
+      .collection("users")
+      .findOne(
+        { _id: new ObjectID("5fe235b00eec89315ca226ab") },
+        (error, user) => {
+          if (error) {
+            return console.log("Unable to fetch user");
+          }
 
-    // db.collection("users").find({ age: 16 }).toArray((error, users) => {
-    //   console.log(users);
-    // });
-    //
-    // db.collection("users").find({ age: 16 }).count((error, count) => {
-    //   console.log(count);
-    // });
+          if (user == null) {
+            return console.log("user not found");
+          }
 
-    // db
-    //   .collection("tasks")
-    //   .findOne(
-    // { _id: new ObjectID("5fe237c51ac3860cccfd68fd") },
-    // (error, task) => {
-    //   if (error) {
-    // return console.log(error);
-    //   }
+          console.log(user);
+        }
+      );
     //
-    //   console.log(task);
-    // }
-    //   );
-    //
-    // db
-    //   .collection("tasks")
-    //   .find({ completed: false })
-    //   .toArray((error, tasks) => {
-    // if (error) {
-    //   return console.log(error);
-    // }
-    //
-    // console.log(tasks);
-    //   });
-    // db
-    // .collection("users")
-    // .updateOne({ name: "Emily" }, { $inc: { age: 1 } })
-    // .then(result => {
-    // console.log(result.result);
-    // })
-    // .catch(error => {
-    // console.log(error);
-    // });
+    db.collection("users").find({ age: 16 }).toArray((error, users) => {
+      console.log(users);
+    });
 
+    db.collection("users").find({ age: 16 }).count((error, count) => {
+      console.log(count);
+    });
+    //
+    db
+      .collection("tasks")
+      .findOne(
+        { _id: new ObjectID("5fe237c51ac3860cccfd68fd") },
+        (error, task) => {
+          if (error) {
+            return console.log(error);
+          }
+
+          console.log(task);
+        }
+      );
+
+    db
+      .collection("tasks")
+      .find({ completed: false })
+      .toArray((error, tasks) => {
+        if (error) {
+          return console.log(error);
+        }
+
+        console.log(tasks);
+      });
+    db
+      .collection("users")
+      .updateOne({ name: "Emily" }, { $inc: { age: 1 } })
+      .then(result => {
+        console.log(result.result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    //
     db
       .collection("tasks")
       .updateMany({ completed: false }, { $set: { completed: true } })
       .then(result => {
         console.log(result.modifiedCount);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    //
+    db
+      .collection("users")
+      .deleteMany({ age: 16 })
+      .then(users => {
+        console.log(users);
+      })
+      .catch(error => {
+        console.log(error);
+        console.error(error);
+      });
+
+    db
+      .collection("users")
+      .deleteOne({ name: "Jude" })
+      .then(user => {
+        console.log(user);
       })
       .catch(error => {
         console.log(error);
